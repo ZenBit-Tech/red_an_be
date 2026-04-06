e0ba1b1d1c26cdc85a061c2b2782a8f14b55a526
Viktor Liulchyk <viktor.lyulchik@gmail.com>
2026-04-06 12:48:07 +0300

    fix: remove unused MailModule import and delete useless controller spec

--- src/modules/example-user/example.user.module.ts
+++ src/modules/example-user/example.user.module.ts
@@ -3,10 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import TemplateUser from '../../common/db/entities/example.user.entity';
 import ExampleUserService from './example.user.service';
 import ExampleUserController from './example.user.controller';
-import MailModule from '../mail/mail.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TemplateUser]), MailModule],
+  imports: [TypeOrmModule.forFeature([TemplateUser])],
   controllers: [ExampleUserController],
   providers: [ExampleUserService],
   exports: [ExampleUserService],
