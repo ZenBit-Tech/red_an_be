f19868b6b42643a4f11ae0ce18ddbb6cc5bb8744
Roman <neirity@gmail.com>
2026-04-08 01:39:59 +0300

    docs: add swagger decorators to contact-us endpoint

--- src/modules/contactForm/contactForm.controller.ts
+++ src/modules/contactForm/contactForm.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Post, Body } from '@nestjs/common';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { ContactFormService } from './contactForm.service';
 import { CreateContactDto } from './dto/contactForm.dto';
 
@@ -7,6 +8,10 @@ export class ContactFormController {
   constructor(private readonly contactFormService: ContactFormService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Send contact us form' })
+  @ApiResponse({ status: 201, description: 'Success' })
+  @ApiResponse({ status: 400, description: 'Bad Request (Validation Error)' })
+  @ApiResponse({ status: 500, description: 'Internal Server Error' })
   async create(@Body() createContactDto: CreateContactDto) {
     return this.contactFormService.handleContactForm(createContactDto);
   }
