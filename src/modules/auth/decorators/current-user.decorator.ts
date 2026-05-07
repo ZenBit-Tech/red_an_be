95125dafc9a7797c7cbed465c7b5c203e06ac9bd
Eduard <nikinna86@gmail.com>
2026-05-07 10:22:43 +0300

    fix:resolve conflicts

--- src/modules/auth/decorators/current-user.decorator.ts
+++ src/modules/auth/decorators/current-user.decorator.ts
@@ -1,19 +1,8 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-<<<<<<< HEAD
 import type { JwtPayload, AuthenticatedRequest } from '../guards/jwt-auth.guard';
 
 export const CurrentUser = createParamDecorator(
   (_data: unknown, ctx: ExecutionContext): JwtPayload => {
-=======
-import { AuthenticatedUser } from '../types/authenticated-user.type';
-
-type AuthenticatedRequest = {
-  user: AuthenticatedUser;
-};
-
-export const CurrentUser = createParamDecorator(
-  (_data: unknown, ctx: ExecutionContext): AuthenticatedUser => {
->>>>>>> develop
     const request = ctx.switchToHttp().getRequest<AuthenticatedRequest>();
     return request.user;
   },
